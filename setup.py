947bf467c48c0bc0983c2efcd4ed67284e036d9e
Ramesh <rameshpythondeveloper@gmail.com>
2020-12-01 08:33:51 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+	name         = 'yuicompressor',
+	version      = '2.4.8',
+	description  = 'A simple wrapper around yuicompressor',
+	author       = 'Ramesh',
+	author_email = 'rameshpythondeveloper@gmail.com',
+	url          = '',
+	download_url = '',
+	py_modules     = (
+		'yuicompressor',
+	)
+)
